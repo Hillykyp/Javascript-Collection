e4dec79b3033cae270273a644980a4987235bd57
HillyKyp <hkngeywo@gmail.com>
2021-01-26 18:30:28 +0000

    Added Dom

--- dom.js
+++ dom.js
@@ -4,19 +4,27 @@ function buttonClick(){
 }
 */
 
-function fn1(){
- var str= document.getElementById("txt1").value;
- let kit = document.getElementById("pss1").value;
- document.write("The username and Password  is:" + str +' ' + kit);
+function fn1() {
+    var str = document.getElementById("txt1").value;
+    let kit = document.getElementById("pss1").value;
+    document.write("The username and Password  is:" + str + ' ' + kit);
 
-} 
-
-
-function fn2(){
-let rd1 = document.getElementById("rd1");
-let rd2 = document.getElementById("rd2");
-let rd3 = document.getElementById("rd3");
-let rd4 = document.getElementById("rd4");
+}
 
 
-}  
+function fn2() {
+    let rd1 = document.getElementById("rd1");
+    let rd2 = document.getElementById("rd2");
+    let rd3 = document.getElementById("rd3");
+    let rd4 = document.getElementById("rd4");
+
+
+}
+
+function congratsMen() {
+    const PI = 3.14;
+    let r = 10;
+    let areCircle = PI * r * r;
+    document.write("The area of a circle is: " + areCircle);
+
+}
